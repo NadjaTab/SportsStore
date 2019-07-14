b97b09703862802b89308a8798643cb3af0d36d5
nadmin <nadmin@MNV-10>
2019-07-14 10:38:05 +0300

    Add project files.

---
+++ SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace SportsStore.Models {
+
+    public interface IProductRepository {
+
+        IEnumerable<Product> Products { get; }
+        void SaveProduct(Product product);
+        Product DeleteProduct(int productID);
+    }
+}
